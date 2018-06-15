90b30d200c2b1c4ea73414d8b297efbdbfeaadda
Eric Birkeland <ericjbirkeland@gmail.com>
2018-06-15 17:02:21 -0400

    init filter show/hide

--- src/js/controllers/filters.ts
+++ src/js/controllers/filters.ts
@@ -1,19 +1,36 @@
 import { ProductType } from "../constants/groups";
 
+
+const productTypes = [1,4,2,5,2,2,2,3,3];
+const productCount = productTypes.length;
+
 var nextActive = ProductType.any;
 var activeFilter = ProductType.any;
-var filters;
+var updateIndex = 0;
+var filters, products;
 
 export function clickFilter(filter: number): boolean {
     if (nextActive === filter) return false;
-    if (!filters) filters = document.querySelectorAll(".filter, .filter--active");
+    if (!filters) {
+        filters = document.querySelectorAll(".filter, .filter--active");
+        products = document.querySelectorAll(".grid__item");
+    }
+    updateIndex = 0;
     nextActive = filter;
     return true;
 }
 
 export function updateFilters(): boolean {
+    var startTime = performance.now();
     filters[activeFilter].className = "filter";
     filters[nextActive].className = "filter--active";
     activeFilter = nextActive;
-    return true;
+    while (updateIndex < productCount) {
+        products[updateIndex].style.display =
+            (activeFilter === 0 || productTypes[updateIndex] === activeFilter) ?
+                "block" : "none";
+        ++updateIndex;
+        if (performance.now() - startTime > 3) break;
+    }
+    return updateIndex === productCount;
 }
